0ef3e7829c38786825a288835b342bfb31876a98
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-07-21 18:32:46 -0400

    Payment microservice to create orders, fetch product info from Inventory service

---
+++ payment/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests, time
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['http://localhost:3000'],
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
+
+redis = get_redis_connection(
+    host = "redis-12823.c14.us-east-1-3.ec2.redns.redis-cloud.com",
+    port = 12823,
+    password = "q88mDnr3YpyGR3OmCwHv4mwBWAegdZFj",
+    decode_responses = True
+)
+
+class Order(HashModel):
+    product_id : str
+    price: float
+    fee: float
+    total : float
+    quantity : int
+    status : str
+
+    class Meta:
+        database = redis
+
+@app.post("/orders")
+async def create(request : Request):
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+
+    product = req.json()
+
+    order = Order(
+        product_id = body['id'],
+        price = product['price'],
+        fee = 0.2 * product['price'],
+        total = 1.2 * product['price'],
+        quantity = body['quantity'],
+        status = 'pending'
+    )
+
+    order.save()
+    order_completed(order)
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = "Completed"
+    order.save()
